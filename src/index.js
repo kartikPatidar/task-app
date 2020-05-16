3cd0734c5b283478ee80c38b4e50ac57efbc61a4
Kartik Patidar <kartikpatidar815@gmail.com>
2020-05-16 20:10:10 +0530

    profile image upload feature added(file uploads)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const auth = require('./middleware/auth')
 
 const app = express()
 const port = process.env.PORT || 3000
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const router = express.Router()
 const auth = require('../middleware/auth') 
 const User = require('../models/user')
@@ -80,6 +82,44 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send(e)
     }
 })
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('not a valid image!'))
+        }
+        cb(undefined, true)
+    }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+    const buffer = await sharp(req.file.buffer).resize(250,250).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
 
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/png')
+        res.send(user.avatar)
+    } catch (e) {
+        res.status(404).send()
+    }
+})
 
 module.exports = router
